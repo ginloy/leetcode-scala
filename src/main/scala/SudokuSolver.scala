0c29bf0dff44dc61f6b8bded4edb6a8fe18eb7ec
ginloy <liyongleow@gmail.com>
2023-03-01 12:17:37 +0800

    Add

--- src/main/scala/SudokuSolver.scala
+++ src/main/scala/SudokuSolver.scala
@@ -10,13 +10,10 @@ object SudokuSolver extends App {
     Sudoku(map).solve match {
       case None => return
       case Some(solved) =>
-        (for {
+        for {
           row <- 0 until 9
           col <- 0 until 9
-        } yield (row, col))
-          .foreach { case (r, c) =>
-            board(r)(c) = solved.get(r, c).getOrElse('.')
-          }
+        } { board(row)(col) = solved.get(row, col).getOrElse('.') }
     }
   }
   case class Sudoku(private val board: Map[(Int, Int), Char]) {
@@ -31,9 +28,9 @@ object SudokuSolver extends App {
       findEmpty match {
         case None => Some(this)
         case Some((i, j)) =>
-          (1 to 9).view
-            .map(d => set(i, j, d.toString.head))
-            .filter(b => b.checkCell(i, j) && b.checkRow(i) && b.checkCol(j))
+          ('1' to '9').view
+            .filter(d => checkRow(i, d) && checkCol(j, d) && checkCell(i, j, d))
+            .map(set(i, j, _))
             .flatMap(_.solve)
             .headOption
       }
@@ -46,16 +43,14 @@ object SudokuSolver extends App {
         if !board.contains((i, j))
       } yield (i, j)).headOption
     }
-    def checkRow(row: Int): Boolean = {
-      val lst = (0 until 9).view.flatMap(board.get(row, _))
-      lst.size == lst.toSet.size
+    def checkRow(row: Int, digit: Char): Boolean = {
+      (0 until 9).view.flatMap(board.get(row, _)).forall(_ != digit)
     }
 
-    def checkCol(col: Int): Boolean = {
-      val lst = (0 until 9).view.flatMap(board.get(_, col))
-      lst.size == lst.toSet.size
+    def checkCol(col: Int, digit: Char): Boolean = {
+      (0 until 9).view.flatMap(board.get(_, col)).forall(_ != digit)
     }
-    def checkCell(row: Int, col: Int): Boolean = {
+    def checkCell(row: Int, col: Int, digit: Char): Boolean = {
       val cellRow = row / 3
       val cellCol = col / 3
       val rowNums = (cellRow * 3 until cellRow * 3 + 3).view
@@ -65,7 +60,7 @@ object SudokuSolver extends App {
         j <- colNums
         c <- board.get((i, j))
       } yield c
-      chars.size == chars.toSet.size
+      chars.forall(_ != digit)
     }
   }
   println(Sudoku(Map()).solve)
